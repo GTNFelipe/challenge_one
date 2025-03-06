c71c99a64029e3cac4961536756a57a8ed8daa2b
Felipe Santana <felipestartt@gmail.com>
2025-03-06 08:45:42 -0300

    sync-work

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
     
-    document.getElementById('adicionarAmigo').addEventListener('click', adicionarAmigo);
+document.getElementById('adicionarAmigo').addEventListener('click', adicionarAmigo);
 
-    function adicionarAmigo() {
-        let nomeAdicionado = document.querySelector("input").value.trim();
+function adicionarAmigo() {
+    let nomeAdicionado = document.querySelector("input").value.trim();
 
-        if (nomeAdicionado === "") {
-         alert("Por favor, insira um nome válido.");
-            return;
-        }
+    if (nomeAdicionado === "") {
+        alert("Por favor, insira um nome válido.");
+        return;     
+    }
     
+    if (amigos.includes(nomeAdicionado)) {
+        alert("Esse amigo já foi adicionado.");
+        return;
+    }
     amigos.push(nomeAdicionado);
-        console.log (amigos);
-     
-        
-    let li = document.createElement("li");
-    li.textContent = nomeAdicionado;
-
-    document.getElementById("listaAmigos").appendChild(li);
+    console.log(amigos);
+    
+    atualizarLista();
 
     document.querySelector("input").value = "";
-    }
+}
 
+function atualizarLista() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    })
+}
 
-const listaAmigos = document.getElementById("lista-Amigos");
+//     document.getElementById("listaAmigos").appendChild(li);
+
+//     document.querySelector("input").value = "";
+//     const listaAmigos = document.getElementById("lista-Amigos");
+//     listaAmigos.InnerHTML = "";
+// }
+
